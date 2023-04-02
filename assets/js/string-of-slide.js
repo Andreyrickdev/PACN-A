a89911dcecc7b8fb3192568f638643f476fc0326
andreyrickdev <contato-andrey@outlook.com>
2023-04-02 16:44:46 -0300

    created setInterval with my Get Element by ID

--- assets/js/string-of-slide.js
+++ assets/js/string-of-slide.js
@@ -2,5 +2,9 @@ function stringOfSlide () {
 
     const phrases = ['Strength','Resistance','Respiration'];
 
+    setInterval( () => {
+        const phrase = document.getElementById('first-phrase');
+    })
+
 }
 stringOfSlide();
