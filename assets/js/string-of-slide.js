fe45b9a223a75867df3de44fec415c6c69dc3757
andreyrickdev <contato-andrey@outlook.com>
2023-04-02 16:55:43 -0300

    created showPhrases function

--- assets/js/string-of-slide.js
+++ assets/js/string-of-slide.js
@@ -3,6 +3,11 @@ function stringOfSlide () {
     const phrases = ['Strength','Resistance','Respiration'];
     const phrase = document.getElementById('first-phrase');
 
+    function showPhrases () {
+        
+    }
+
+
     setInterval( () => {
     })
 
